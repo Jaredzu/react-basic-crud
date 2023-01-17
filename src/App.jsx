aac9be97e577e0dabc207f1e8f0a98b6ecfef25a
Jaredzu <jared.zu96@gmail.com>
2023-01-17 14:37:10 -0500

    feat: added basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 // Importación de  Librerías y otros archivos
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Navbar from './Navbar'
+
+//Declarar Router
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Navbar />} >
+
+        </Route>
+    )
+)
+
+
 
 // Declarar componente
 const App = () => {
-  return (
-    <p>Soy App!</p>
-  )
+    return (
+        <RouterProvider router={router} />
+    )
 }
 // Exportamos el componente
 export default App // Default export JS
