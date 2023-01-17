1c20d0a0781c26b409602a80e676a3ad57d536bc
Jaredzu <jared.zu96@gmail.com>
2023-01-17 14:25:25 -0500

    Feat: Basic template React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// Importación de  Librerías y otros archivos
+import React from 'react'
+
+// Declarar componente
+const App = () => {
+  return (
+    <p>Soy App!</p>
+  )
+}
+// Exportamos el componente
+export default App // Default export JS
+
