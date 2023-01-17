dbee0b58070712f7ab3a8fa656718895f3e0ceeb
Jaredzu <jared.zu96@gmail.com>
2023-01-17 14:47:31 -0500

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // Importación de  Librerías y otros archivos
 import React from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Admin from './Admin'
+import Home from './Home'
 import Navbar from './Navbar'
+
 
 //Declarar Router
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<Navbar />} >
-
+            <Route path='/' element={<Home />} />
+            <Route path='/admin' element={<Admin />} />
         </Route>
     )
 )
