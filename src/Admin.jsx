9ad022b25d40909b750c8d37f77bd0cf45bf6589
Jaredzu <jared.zu96@gmail.com>
2023-01-17 14:58:09 -0500

    feat: added basic form

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import UserForm from './components/UserForm'
 
 const Admin = () => {
     return (
-        <h1>Admin</h1>
+        <>
+            <h1>Admin</h1>
+            <div className="p-1">
+                <UserForm />
+            </div>
+        </>
     )
 }
 
